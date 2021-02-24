65d9746bb3ee4567254afff592c31088a94118d8
Gabrieli Bernardi <gabrieli.bernardi@gmail.com>
2021-02-24 20:30:24 -0300

    implementa styled components

---
+++ src/components/Card.js
@@ -0,0 +1,72 @@
+import { useHistory } from "react-router-dom";
+import {
+  Typography,
+  Box,
+  Card as CardWrapper,
+  CardContent,
+} from "@material-ui/core";
+import styled from "styled-components";
+import ShoppingButton from "./ShoppingButton";
+
+const BoxWrapper = styled(CardWrapper)`
+  width: 258px;
+  height: 448px;
+`;
+
+const CardImageContent = styled(Box)`
+  text-align: center;
+`;
+const CardImg = styled.img`
+  width: 226px;
+  height: 226px;
+  padding-top: 16px;
+`;
+const CardContentStyled = styled(CardContent)`
+  display: flex;
+  flex-direction: column;
+`;
+
+function Card({ product }) {
+  const installmentsPayment = 10;
+  const history = useHistory();
+
+  return (
+    <BoxWrapper key={product.id}>
+      <CardImageContent>
+        <CardImg
+          src={product.image}
+          alt="imagem"
+          onClick={() => history.push("/detalhes")}
+        />
+      </CardImageContent>
+      <CardContentStyled>
+        <Box mb={2}>
+          <Typography variant="h4" onClick={() => history.push("/detalhes")}>
+            {product.name}
+          </Typography>
+        </Box>
+        <Typography variant="h3" gutterBottom>
+          {product.price.toLocaleString("pt-BR", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+            //style: "currency",
+            currency: "BRL",
+          })}
+        </Typography>
+        <Typography variant="body1">
+          ou {installmentsPayment}x de{" "}
+          {(product.price / installmentsPayment).toLocaleString("pt-BR", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+            //style: "currency",
+            currency: "BRL",
+          })}
+        </Typography>
+        <Box mb={2}></Box>
+        <ShoppingButton />
+      </CardContentStyled>
+    </BoxWrapper>
+  );
+}
+
+export default Card;
