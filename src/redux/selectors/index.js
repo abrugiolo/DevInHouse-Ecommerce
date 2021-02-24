31983a678fa686f43dde1728499436a95181def7
manubf <emanuellebf@gmail.com>
2021-02-23 21:03:09 -0300

    implementa redux

---
+++ src/redux/selectors/index.js
@@ -0,0 +1,3 @@
+export const getInputSearch = (store) => store.searchReducer.inputSearch;
+export const getProductsList = (store) => store.cartReducer.productsList;
+export const getProductDetail = (store) => store.detailReducer.productDetail;
