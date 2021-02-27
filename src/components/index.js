d9ca0175203c2c033ee67d281865e4358d0f38ef
Milena Diniz Vieira <miladvieira@outlook.com>
2021-02-26 21:56:02 -0300

    adiciona estilização de detalhes no card

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,5 @@ export { default as Header } from "./Header";
 export { default as InputSearch } from "./InputSearch";
 export { default as CartButton } from "./CartButton";
 export { default as PageWrapper } from "./PageWrapper";
-export { default as CardDetail } from "./CardDetail";
-export {default as ButtonAddRemove } from "./ButtonAddRemove";
+export { default as ButtonAddRemove } from "./ButtonAddRemove";
 export { default as ButtonShop } from "./ButtonShop";
